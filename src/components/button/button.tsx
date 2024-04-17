d0f0f94aba8751bd8d803cd72542aec27df35dc0
Brent Swisher <brent.swisher@ithaka.org>
2024-04-17 11:39:02 -0400

    Add button variants and stories

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,17 @@
-export const Button = (props: any) => {
-  return <button {...props}>{props.children}</button>;
+import { ComponentProps } from 'react';
+
+import styles from './button.module.css';
+
+export type ButtonProps = ComponentProps<'button'> & {
+  variant: 'primary' | 'secondary' | 'destructive';
+};
+
+export const Button = ({ variant, ...props }: ButtonProps) => {
+  let className = styles.button;
+  if (variant === 'secondary') {
+    className += ` ${styles.secondary}`;
+  } else if (variant === 'destructive') {
+    className += ` ${styles.destructive}`;
+  }
+  return <button className={className} {...props} />;
 };
